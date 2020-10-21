157ee96681017c5ead786d20c1ec16206a2fd7d0
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2020-10-21 09:01:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,18 @@ def on_chat_message(msg):
 	keyboard = InlineKeyboardMarkup(inline_keyboard=[
 		[InlineKeyboardButton(text="Click here", callback_data='press')],
 		])
+	
+	bot.sendMessage(chat_id, "Utilizza la inline keyboard", reply_markup=keyboard)
+
+def on_callback_query(msg):
+	query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+	print("Callback Query: ", query_id, from_id, query_data)
+	
+	bot.answerCallbackQuery(query_id, text="Ok!")
 
 	
-bot.message_loop(handle)
+bot.message_loop({'chat': on_chat_message,
+		  'callback_query': on_callback_query})
 
 response = bot.getUpdates()
 pprint(response)
