6381686437f92c40ba387e21474c26b734ca1383
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2021-01-21 09:49:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ import telepot
 import sys, time
 from pprint import pprint
 
+# Il file si integra a "main.py"
+
 TOKEN = "inserire token qui"
-ONLINE = true
-SEND_MSG = true
+ONLINE = true # Bot acceso/spento 
+SEND_MSG = true # Bot abilitato ad invio messaggi
 keyboard = 'inline'
 
